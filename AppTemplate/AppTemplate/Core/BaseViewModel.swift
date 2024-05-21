52a8389dbc03c5e7c9ba4fbbb7730f21822d4d3d
miguelsolans <dev.miguelsolans@gmail.com>
2024-05-21 22:57:04 +0100

    Add BaseViewModel class with protocol

---
+++ AppTemplate/AppTemplate/Core/BaseViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  BaseViewModel.swift
+//  AppTemplate
+//
+//  Created by Miguel Solans on 21/05/2024.
+//
+
+import Foundation
+
+/// Use this protocol to define methods a coordiantor delegate must comply to
+protocol ViewModelCoordinatorDelegate: AnyObject { }
+
+/// Use this protocol to define methods a view delegate must comply to
+protocol ViewModelDelegate: AnyObject { }
+
+/// Base class for a ViewModel
+class BaseViewModel<CoordinatorDelegateType: ViewModelCoordinatorDelegate, ViewDelegateType: ViewModelDelegate> {
+    
+    /// Set delegate with coordinator class
+    weak var coordinatorDelegate: CoordinatorDelegateType?
+    
+    /// Set delegate with view class
+    weak var viewDelegate: ViewDelegateType?
+    
+    /// Initializer for a ViewModel base class
+    /// - Parameters:
+    ///   - coordinatorDelegate: coordinator delegate class
+    ///   - viewDelegate: view delegate class
+    init(coordinatorDelegate: CoordinatorDelegateType?, viewDelegate: ViewDelegateType?) {
+        self.coordinatorDelegate = coordinatorDelegate
+        self.viewDelegate = viewDelegate
+    }
+    
+}
---
+++ AppTemplate/AppTemplate/Modules/Sample Feature B/View/SampleFeatureBViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SampleFeatureBViewController.swift
+//  AppTemplate
+//
+//  Created by Miguel Solans on 21/05/2024.
+//
+
+import UIKit
+
+class SampleFeatureBViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+}
